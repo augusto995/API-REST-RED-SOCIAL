83b94e1f76fbc4b4ba218d14ef22820ad158e219
Augusto Ana <augustora95@gmail.com>
2024-10-31 15:39:00 -0300

    47. Script para ahorrar tiempo

---
+++ index.js
@@ -0,0 +1,13 @@
+//Conexion a la base de datos
+
+
+//Crear servidor de node
+
+//Configurar el cors
+
+
+//Convertir datos del body a obj js
+
+//Cargar configuracion de rutas
+
+//Poner servidor a escuchar peticiones http
