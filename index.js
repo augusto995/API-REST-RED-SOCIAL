95b27db959b84553fb76ebb3d55850c51d15800d
Augusto Ana <augustora95@gmail.com>
2024-11-01 16:21:19 -0300

    48. Conexion a mongo

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+//Importar dependencias
+const connection = require("./database/connection")
+
+//Mensaje bienvenida
+
+console.log("Api Node para red social arrancada")
+
+
 //Conexion a la base de datos
-//
+connection();
 
 //Crear servidor de node
 
-//Configurar el cors
+//Configurar el corss
 
 
 //Convertir datos del body a obj js
