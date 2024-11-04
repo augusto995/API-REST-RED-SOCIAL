302d08cab56ad36bf5bf0addb30e1a2ccd3d4283
Augusto Ana <augustora95@gmail.com>
2024-11-04 12:59:19 -0300

    49. Configurando el servidor de node

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //Importar dependencias
 const connection = require("./database/connection")
+const express = require("express")
+const cors = require("cors")
 
 //Mensaje bienvenida
 
@@ -10,12 +12,30 @@ console.log("Api Node para red social arrancada")
 connection();
 
 //Crear servidor de node
+const app = express();
+const puerto = 3900;
 
 //Configurar el corss
-
+app.use(cors());
 
 //Convertir datos del body a obj js
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 //Cargar configuracion de rutas
+//Ruta de prueba
+app.get("ruta-prueba", (req, res) =>{
+    return res.status(200).json(
+        {
+            "id": 1,
+            "nombre": "Augusto",
+            "web": "augusto.es"
+        }
+    )
+})
 
 //Poner servidor a escuchar peticiones http
+
+app.listen(puerto, () => {
+    console.log("Servidor de node corriendo en el puerto: ", puerto);
+});
