7bde83d1b2e9255c4f39214e803e7d932c632c64
Augusto Ana <augustora95@gmail.com>
2024-11-05 12:36:11 -0300

    50. Creando controladores de prueba

---
+++ controllers/follow.js
@@ -0,0 +1,12 @@
+//Acciones de prueba
+
+const pruebaFollow = (req,res) => {
+    return res.status(200).send({
+        message: "Mensaje enviado desde: controller/user.js"
+    })
+}
+
+//Exportar Acciones
+module.exports = {
+    pruebaFollow
+}
---
+++ controllers/user.js
@@ -0,0 +1,12 @@
+//Acciones de prueba
+
+const pruebaUser = (req,res) => {
+    return res.status(200).send({
+        message: "Mensaje enviado desde: controller/user.js"
+    })
+}
+
+//Exportar Acciones
+module.exports = {
+    pruebaUser
+}
