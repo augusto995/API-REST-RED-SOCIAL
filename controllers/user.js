a052f81d0b45decdc8a6df1351ba68058e206e7b
Augusto Ana <augustora95@gmail.com>
2024-11-07 13:42:23 -0300

    53. Accion y ruta de registro de usuarios

--- controllers/user.js
+++ controllers/user.js
@@ -6,7 +6,22 @@ const pruebaUser = (req,res) => {
     })
 }
 
+//Registro de usuarios
+
+const register = (req, res) => {
+    //Recoger datos de la peticion
+    //Comprobar que me llegan + validacion
+    //Comprobar usuarios duplicados
+    //Cifrar la contraseña
+    //Guardar usuario en la bdd
+    //Devolver resultado
+    return res.status(200).json({
+        message: "Accion de registro de usuarios"
+    });
+}
+
 //Exportar Acciones
 module.exports = {
-    pruebaUser
+    pruebaUser,
+    register
 }
